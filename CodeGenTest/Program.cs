5f1deaef8462dcb6a98eac757006fdeef13b5e93
Benjamin Boyle <bboyle1234@gmail.com>
2019-11-02 00:49:28 -0400

    Initial commit

---
+++ CodeGenTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using Amadevus.RecordGenerator;
+using System;
+
+namespace CodeGenTest {
+
+    [Record]
+    sealed partial class SomeObject {
+        public int SomeValue { get; }
+    }
+
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+            var x = new SomeObject(10);
+        }
+    }
+}
